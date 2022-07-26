b34298c56f9888573ed37f6b8f43df78a5403e52
russell <rjhua@cpp.edu>
2022-07-25 22:07:59 -0700

    cleans up the two methods of sending data adds missing configurations for gpio pins

--- Code/pioTest/pioTest.c
+++ Code/pioTest/pioTest.c
@@ -10,8 +10,7 @@ int main()
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &test_program);
     uint sm = pio_claim_unused_sm(pio, true);
-    // pin(s) configurated here: want din to gp18, wsel gp26, clk to gp27
-    // in the future, the wsel and clk together can be set to arbitrary pins, but for now, they are consecutive
+    // pin(s) configurated here: want din to gp18, wsel gp26, bclk to gp27
     test_program_init(pio, sm, offset, 18, 26);
     // A C note is 261.63hz or a period of 0.003822191645s
     const double C_PERIOD_MS = (1 / 261.63) * 1000;
@@ -28,29 +27,28 @@ int main()
     // put the values twice in input because of using mono audio - 16 bits for left, 16 bits for right
     while (true)
     {
-        absolute_time_t current = get_absolute_time();
-        while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)((C_PERIOD_MS / 2) * 1000))
-        {
-            int32_t left = (MAX_VALUE_I2S << 16u) & 0xFFFF0000;
-            int32_t right = MAX_VALUE_I2S & 0x0000FFFF;
-            pio_sm_put_blocking(pio, sm, (left | right));
-            printf("Sending positive %X\n", (left | right));
-            //     pio_sm_put_blocking(pio, sm, ((MAX_VALUE_I2S / 2)));
-        }
-        // pio_sm_put_blocking(pio, sm, ((MAX_VALUE_I2S << 16u)));
-        // sleep_ms(C_PERIOD_MS / 2);
-        current = get_absolute_time();
-        while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)((C_PERIOD_MS / 2) * 1000))
-        {
-            int32_t left = -((MAX_VALUE_I2S << 16u) & 0xFFFF0000);
-            int32_t right = -MAX_VALUE_I2S & 0x0000FFFF;
-            pio_sm_put_blocking(pio, sm, (left | right));
-            printf("Sending positive %X\n", (left | right));
-            //     pio_sm_put_blocking(pio, sm, (-((MAX_VALUE_I2S / 2))));
-        }
-        // pio_sm_put_blocking(pio, sm, -(((MAX_VALUE_I2S) / 2)) << 16u | -((MAX_VALUE_I2S) / 2));
-        // pio_sm_put_blocking(pio, sm, (-MAX_VALUE_I2S << 16u));
-        // sleep_ms(C_PERIOD_MS / 2);
+        int32_t left = (MAX_VALUE_I2S << 16u) & 0xFFFF0000;
+        int32_t right = MAX_VALUE_I2S & 0x0000FFFF;
+        printf("Sending positive %X\n", (left | right));
+        pio_sm_put_blocking(pio, sm, (left | right));
+        sleep_ms(C_PERIOD_MS / 2);
+        // absolute_time_t current = get_absolute_time();
+        // while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)((C_PERIOD_MS / 2) * 1000))
+        // {
+        //     pio_sm_put_blocking(pio, sm, (left | right));
+        //     //     pio_sm_put_blocking(pio, sm, ((MAX_VALUE_I2S / 2)));
+        // }
+        left = -((MAX_VALUE_I2S << 16u) & 0xFFFF0000);
+        right = -MAX_VALUE_I2S & 0x0000FFFF;
+        printf("Sending positive %X\n", (left | right));
+        pio_sm_put_blocking(pio, sm, (left | right));
+        sleep_ms(C_PERIOD_MS / 2);
+        // current = get_absolute_time();
+        // while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)((C_PERIOD_MS / 2) * 1000))
+        // {
+        //     pio_sm_put_blocking(pio, sm, (left | right));
+        //     //     pio_sm_put_blocking(pio, sm, (-((MAX_VALUE_I2S / 2))));
+        // }
     }
     return 0;
 }
