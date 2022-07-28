25b5c794e54c1f4748c05c7ba6ca7a36dc4ee500
russell <rjhua@cpp.edu>
2022-07-28 14:08:30 -0700

    audio works!!! two main errors: wsel and bclk were switched pio frequency was too fast: 64/sample rate vs correct 32/sample rate

--- Code/pioTest/pioTest.c
+++ Code/pioTest/pioTest.c
@@ -7,45 +7,59 @@
 // TODO: more debug info?
 void sendLoop(const int16_t MAX_VALUE_I2S, const double C_PERIOD_MS, PIO pio, uint sm)
 {
-    int32_t left = (MAX_VALUE_I2S << 16u) & 0xFFFF0000;
-    int32_t right = MAX_VALUE_I2S & 0x0000FFFF;
+    int32_t right = (MAX_VALUE_I2S << 16u) & 0xFFFF0000;
+    int32_t left = MAX_VALUE_I2S & 0x0000FFFF;
     const double SAMPLING_PERIOD_MS = (double)1 / 44100 * 1000;
     for (size_t i = 0; i < (size_t)((C_PERIOD_MS / SAMPLING_PERIOD_MS) / 2); i++)
     {
-        pio_sm_put_blocking(pio, sm, (left | right));
-        printf("Sending positive %X\n", (left | right));
+        pio_sm_put_blocking(pio, sm, (right | left));
+        printf("Sending positive %X\n", (right | left));
     }
-    left = -((MAX_VALUE_I2S << 16u) & 0xFFFF0000);
-    right = -MAX_VALUE_I2S & 0x0000FFFF;
+    right = -((MAX_VALUE_I2S << 16u) & 0xFFFF0000);
+    left = -MAX_VALUE_I2S & 0x0000FFFF;
     for (size_t i = 0; i < (size_t)((C_PERIOD_MS / SAMPLING_PERIOD_MS) / 2); i++)
     {
-        pio_sm_put_blocking(pio, sm, (left | right));
-        printf("Sending negative %X\n", (left | right));
+        pio_sm_put_blocking(pio, sm, (right | left));
+        printf("Sending negative %X\n", (right | left));
     }
 }
-void sendTimer(const int16_t MAX_VALUE_I2S, const int16_t C_PERIOD_MS, PIO pio, uint sm)
+void sendTimer(const int16_t MAX_VALUE_I2S, const double C_PERIOD_MS, PIO pio, uint sm)
 {
-    int32_t left = (MAX_VALUE_I2S << 16u) & 0xFFFF0000;
-    int32_t right = MAX_VALUE_I2S & 0x0000FFFF;
-    // pio_sm_put_blocking(pio, sm, (left | right));
+    int32_t right = (MAX_VALUE_I2S << 16u) & 0xFFFF0000;
+    int32_t left = MAX_VALUE_I2S & 0x0000FFFF;
+    left = 0x00000000;
+    uint32_t leftRightData = (uint32_t)right | (uint32_t)left;
+    // pio_sm_put_blocking(pio, sm, leftRightData);
     // sleep_ms(C_PERIOD_MS / 2);
+    pio_sm_put_blocking(pio, sm, leftRightData);
     absolute_time_t current = get_absolute_time();
-    while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)((C_PERIOD_MS / 2) * 1000))
+    // absolute_time_t pioTime;
+    while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)(((C_PERIOD_MS * 1000) / 2)))
     {
-        // printf("Sending positive %X\n", (left | right));
-        pio_sm_put_blocking(pio, sm, (left | right));
-        //     pio_sm_put_blocking(pio, sm, ((MAX_VALUE_I2S / 2)));
+        printf("Sending positive %X\n", leftRightData);
+        // pioTime = get_absolute_time();
+        pio_sm_put_blocking(pio, sm, leftRightData);
+        // printf("Pio time is %lld\n", (absolute_time_diff_us(pioTime, get_absolute_time())));
+        // printf("Time used is: %lld\n", (absolute_time_diff_us(current, get_absolute_time())));
     }
-    left = -((MAX_VALUE_I2S << 16u) & 0xFFFF0000);
-    right = -MAX_VALUE_I2S & 0x0000FFFF;
-    // pio_sm_put_blocking(pio, sm, (left | right));
+    right = -((MAX_VALUE_I2S << 16u) & 0xFFFF0000);
+    left = -MAX_VALUE_I2S & 0x0000FFFF;
+    left = 0x00000000;
+    leftRightData = (uint32_t)right | (uint32_t)left;
+    // pio_sm_put_blocking(pio, sm, leftRightData);
     // sleep_ms(C_PERIOD_MS / 2);
+    pio_sm_put_blocking(pio, sm, leftRightData);
     current = get_absolute_time();
-    while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)((C_PERIOD_MS / 2) * 1000))
+    // current problem: I think autopull pulls partially and not the whole 32 bit value
+    // it's almost like it's sending only the left channel
+    // the first 3 bits do change
+    while (absolute_time_diff_us(current, get_absolute_time()) < (int64_t)(((C_PERIOD_MS * 1000) / 2)))
     {
-        // printf("Sending negative %X\n", (left | right));
-        pio_sm_put_blocking(pio, sm, (left | right));
-        //     pio_sm_put_blocking(pio, sm, (-((MAX_VALUE_I2S / 2))));
+        printf("Sending negative %X\n", leftRightData);
+        // pioTime = get_absolute_time();
+        pio_sm_put_blocking(pio, sm, leftRightData);
+        // printf("Pio time is %lld\n", (absolute_time_diff_us(pioTime, get_absolute_time())));
+        // printf("Time used is: %lld\n", (absolute_time_diff_us(current, get_absolute_time())));
     }
 }
 int main()
@@ -56,6 +70,7 @@ int main()
     uint offset = pio_add_program(pio, &test_program);
     uint sm = pio_claim_unused_sm(pio, true);
     // pin(s) configurated here: want din to gp18, wsel gp26, bclk to gp27
+    // problem: wsel is gp27, bclk is gp26
     test_program_init(pio, sm, offset, 18, 26);
     // A C note is 261.63hz or a period of 0.003822191645s
     const double C_PERIOD_MS = ((double)1 / 261.63) * 1000;
