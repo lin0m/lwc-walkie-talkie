39decd504b662b9054544e99da197669bc3a0a8a
russell <rjhua@cpp.edu>
2022-07-29 20:59:19 -0700

    fixes some issues with pio program adds some output for data in mic program fixes serial over uart so it works

--- Code/mic/micTest.c
+++ Code/mic/micTest.c
@@ -5,7 +5,7 @@
 
 /**
  * @brief this adds data to the end of a uint32_t array
- * 
+ *
  * @param data data to insert
  * @param arr array containing data
  * @param size current size of the array - passed as reference
@@ -13,33 +13,41 @@
  * @return true - the add was successful
  * @return false - the add was unsuccessful - array was full;
  */
-bool addArray(uint32_t data, uint32_t* arr, size_t* size, size_t capacity) {
-    if (*size < capacity) {
+bool addArray(uint32_t data, uint32_t *arr, size_t *size, size_t capacity)
+{
+    if (*size < capacity)
+    {
         arr[*size++] = data;
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 int main()
 {
     stdio_init_all();
+    sleep_ms(10000);
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &mic_program);
     uint sm = pio_claim_unused_sm(pio, true);
-    mic_program_init(pio, sm, offset, 18, 26);
+    mic_program_init(pio, sm, offset, 18, 12);
+    printf("program initialized\n");
     size_t size = 0;
     // provide half a second of buffer
     const size_t CAPACITY = 44100 / 2;
     uint32_t buffer[CAPACITY];
-    while (true) {
+    while (true)
+    {
         uint32_t lrData;
         lrData = pio_sm_get_blocking(pio, sm);
-        printf("data is: %X", lrData);
+        printf("data is: %032X\t", lrData);
         // if unsuccessful, wait until buffer is not full
-        if (!addArray(lrData, buffer, &size, CAPACITY)) {
-            sleep_ms(1);
-        }
+        // if (!addArray(lrData, buffer, &size, CAPACITY))
+        // {
+        //     sleep_ms(1);
+        // }
     }
     return 0;
 }
