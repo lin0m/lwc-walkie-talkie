bd97432f2b8cfc2d60496dda912a5f232baf321e
russell <rjhua@cpp.edu>
2022-07-30 02:53:15 -0700

    adds dac to micTest code; pretty messy mainly to test i2s from "virtual" transmitter to receiver

--- Code/mic/micTest.c
+++ Code/mic/micTest.c
@@ -2,48 +2,97 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "mic.pio.h"
-
-/**
- * @brief this adds data to the end of a uint32_t array
- *
- * @param data data to insert
- * @param arr array containing data
- * @param size current size of the array - passed as reference
- * @param capacity total size of array
- * @return true - the add was successful
- * @return false - the add was unsuccessful - array was full;
- */
-bool addArray(uint32_t data, uint32_t *arr, size_t *size, size_t capacity)
+#include "dac.pio.h"
+// /**
+//  * @brief this adds data to the end of a uint32_t array
+//  *
+//  * @param data data to insert
+//  * @param arr array containing data
+//  * @param size current size of the array - passed as reference
+//  * @param capacity total size of array
+//  * @return true - the add was successful
+//  * @return false - the add was unsuccessful - array was full;
+//  */
+// bool addArray(uint32_t data, uint32_t *arr, size_t *size, size_t capacity)
+// {
+//     if (*size < capacity)
+//     {
+//         arr[*size++] = data;
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+void sendTimer(const int16_t MAX_VALUE_I2S, const double C_PERIOD_MS, PIO pio, uint sm, absolute_time_t* current)
 {
-    if (*size < capacity)
+    // pio_sm_put_blocking(pio, sm, leftRightData);
+    // sleep_ms(C_PERIOD_MS / 2);
+    // absolute_time_t pioTime;
+    uint32_t leftRightData;
+    int32_t right;
+    int32_t left;
+    if (absolute_time_diff_us(*current, get_absolute_time()) < (int64_t)(((C_PERIOD_MS * 1000) / 2)))
     {
-        arr[*size++] = data;
-        return true;
-    }
-    else
-    {
-        return false;
+        right = (MAX_VALUE_I2S << 16u) & 0xFFFF0000;
+        left = MAX_VALUE_I2S & 0x0000FFFF;
+        leftRightData = (uint32_t)right | (uint32_t)left;
+        // printf("Sending positive %X\n", leftRightData);
+        // pioTime = get_absolute_time();
+        pio_sm_put_blocking(pio, sm, leftRightData);
+        // printf("Pio time is %lld\n", (absolute_time_diff_us(pioTime, get_absolute_time())));
+        // printf("Time used is: %lld\n", (absolute_time_diff_us(current, get_absolute_time())));
+    } else if (absolute_time_diff_us(*current, get_absolute_time()) < (int64_t)(((C_PERIOD_MS * 1000)))) {
+        right = -((MAX_VALUE_I2S << 16u) & 0xFFFF0000);
+        left = -MAX_VALUE_I2S & 0x0000FFFF;
+        leftRightData = (uint32_t)right | (uint32_t)left;
+        // pio_sm_put_blocking(pio, sm, leftRightData);
+        // sleep_ms(C_PERIOD_MS / 2);
+        pio_sm_put_blocking(pio, sm, leftRightData);
+        // printf("Sending negative %X\n", leftRightData);
+        // pioTime = get_absolute_time();
+        // printf("Pio time is %lld\n", (absolute_time_diff_us(pioTime, get_absolute_time())));
+        // printf("Time used is: %lld\n", (absolute_time_diff_us(current, get_absolute_time())));
+    } else {
+        *current = get_absolute_time();
     }
 }
+
 int main()
 {
     stdio_init_all();
-    sleep_ms(10000);
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%d\n", i);
+        sleep_ms(1000);
+    }
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &mic_program);
     uint sm = pio_claim_unused_sm(pio, true);
-    mic_program_init(pio, sm, offset, 18, 10);
+    mic_program_init(pio, sm, offset, 18, 26);
+
+    PIO pio_dac = pio1;
+    uint offset_dac = pio_add_program(pio_dac, &dac_program);
+    uint sm_dac = pio_claim_unused_sm(pio_dac, true);
+    dac_program_init(pio_dac, sm_dac, offset_dac, 13, 6);
     uint32_t lrData;
-    printf("program initialized\n");
+    // const double MAX_FREQUENCY = (double)44100 / 2;
+    // const double MAX_PERIOD = 1 / MAX_FREQUENCY;
+    // const double MAX_PERIOD_US = MAX_PERIOD * 1000000;
     // size_t size = 0;
     // provide half a second of buffer
     // const size_t CAPACITY = 44100 / 2;
     // uint32_t buffer[CAPACITY];
+    const int16_t MAX_VALUE_I2S = (32767);
+    const double C_PERIOD_MS = ((double)1 / 261.63) * 1000;
+    absolute_time_t current = get_absolute_time();
     while (true)
     {
+        sendTimer(MAX_VALUE_I2S, C_PERIOD_MS, pio_dac, sm_dac, &current);
         lrData = pio_sm_get_blocking(pio, sm);
-        printf("data is: %08X\n", lrData);
-        sleep_ms(10);
+        printf("data is: %X\n", lrData);
+        // sleep_ms(10);
         // if unsuccessful, wait until buffer is not full
         // if (!addArray(lrData, buffer, &size, CAPACITY))
         // {
