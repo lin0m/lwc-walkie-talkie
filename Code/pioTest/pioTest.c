8c705d30113da1da9de6819eb6beee065b10ef8b
Lino Mercado-Esquivias <lino@b98-aruba-authenticated-10-110-156-165.wlan.cpp.edu>
2022-08-09 10:57:23 -0700

    Added boiler plate comments

--- Code/pioTest/pioTest.c
+++ Code/pioTest/pioTest.c
@@ -1,3 +1,22 @@
+/*  Project:        Audio Encryption Project
+    Description:    Element 14 Pi-Fest challenge submission. System comprised of three Raspberry Pi Picos 
+                    named Bob, Alice, and Server. System securely sends audio between Bob and Alice using 
+                    a lightweight cryptographic algorithm. Keys are exchanged using X3DH. Bob receives audio 
+                    as input, encrypts it, and sends it to the server. Alice receives the message, decrypts
+                    it, and plays the audio.
+    Documentation:  https://github.com/lin0m/lightweight-cryptography
+    Date:           Aug 9th, 2022
+    School:         Cal Poly Pomona 
+    Group:          Dr. Aly's Research Team
+    Members:        Melvin Relf
+                    Russell Hua
+                    Alexander Ea
+                    Shahzman Saqib
+                    Muhammed Kamil
+                    Patricia Ankunda
+                    Lino Mercado-Esquivias
+*/
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
--- Code/server/server.c
+++ Code/server/server.c
@@ -1,3 +1,22 @@
+/*  Project:        Audio Encryption Project
+    Description:    Element 14 Pi-Fest challenge submission. System comprised of three Raspberry Pi Picos 
+                    named Bob, Alice, and Server. System securely sends audio between Bob and Alice using 
+                    a lightweight cryptographic algorithm. Keys are exchanged using X3DH. Bob receives audio 
+                    as input, encrypts it, and sends it to the server. Alice receives the message, decrypts
+                    it, and plays the audio.
+    Documentation:  https://github.com/lin0m/lightweight-cryptography
+    Date:           Aug 9th, 2022
+    School:         Cal Poly Pomona 
+    Group:          Dr. Aly's Research Team
+    Members:        Melvin Relf
+                    Russell Hua
+                    Alexander Ea
+                    Shahzman Saqib
+                    Muhammed Kamil
+                    Patricia Ankunda
+                    Lino Mercado-Esquivias
+*/
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include <string.h>
@@ -16,16 +35,20 @@ int main()
     int aliceID;
     int bobID;
 
-    /*----------------------------------WAIT FOR MESSAGE-----------------------------------*/
+    /*---------------WAIT FOR MESSAGE---------------*/
 
-    /*---------------------------------CHECK MESSAGE TYPE----------------------------------*/
+    /*--------------CHECK MESSAGE TYPE--------------*/
     switch(msg_type){
+    /*--------------------TYPE 0--------------------*/
         case 0:
         break;
+    /*--------------------TYPE 1--------------------*/
         case 1:
         break;
+    /*--------------------TYPE 2--------------------*/
         case 2:
         break;
+    /*--------------------TYPE 3--------------------*/
         case 3;
         break;
         default:
@@ -34,19 +57,18 @@ int main()
     return 0;
 }
 
-/*---------------------------------------TYPE 0----------------------------------------*/
 void type0(message){
 
 }
-/*---------------------------------------TYPE 1----------------------------------------*/
+
 void type1(message){
-        
+
 }
-/*---------------------------------------TYPE 2----------------------------------------*/
+
 void type2(message){
-        
+
 }
-/*---------------------------------------TYPE 3----------------------------------------*/
+
 void type3(message){
-        
+
 }
